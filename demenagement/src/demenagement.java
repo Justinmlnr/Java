623d5743d42a819b92b5dd9a63489bf8dd1586c0
Justinmlnr <juju.vlb38@gmail.com>
2026-02-27 09:46:32 +0100

    demenagement

---
+++ demenagement/src/demenagement.java
@@ -0,0 +1,8 @@
+public class demenagement {
+}
+
+// cartons = 34
+// capacité camion = 9 cartons
+// total de cartons et contenance du camion donnés en valeurs " en dur "
+// afficher pour chaque voyage effectué le message " un voyage de x cartons"
+// sois 9+9+9+7 = 34 cartons = 4 voyages
